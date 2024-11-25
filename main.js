2f83fbfee13c58a1c8621f15870d60af51e48532
Tomer <tomera.almog9@gmail.com>
2024-11-25 22:01:06 +0200

    Init repo + start of app -up to get camera input

---
+++ main.js
@@ -0,0 +1,74 @@
+// Prefer camera resolution nearest to 1280x720.
+const constraints = {
+  audio: true,
+  video: {
+    width: 1280,
+    height: 720,
+    frameRate: { ideal: 30, max: 120 },
+    deviceId: "",
+  },
+};
+
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then((mediaStream) => {
+    console.log("this is a media stream: ", mediaStream);
+    const video = document.querySelector("video");
+    console.log("this is a video ele: ", video);
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  })
+  .catch((err) => {
+    // always check for errors at the end.
+    console.error(`${err.name}: ${err.message}`);
+  });
+
+const devices = navigator.mediaDevices
+  .enumerateDevices()
+  .then((devices) => {
+    //got the devices
+    //get video inputs, get their names and put in the select element
+    console.log(devices);
+    const videoInputs = devices.filter(
+      (device) => device.kind === "videoinput"
+    );
+    //render them
+
+    const elSelect = document.querySelector(".cam-select");
+    console.log(videoInputs);
+    var strHTML = videoInputs.map((input) => {
+      return `
+          <option value="${input.deviceId}">${input.label}</option>
+        `;
+    });
+    console.log("str html", strHTML.join(""));
+    elSelect.innerHTML = strHTML;
+  })
+  .catch((err) => console.log("this is your error: ", err));
+
+function onSetCamera(id) {
+  console.log("camera id: ", id);
+  getMediaStream(id);
+}
+
+function getMediaStream(id) {
+  constraints.video.deviceId = id;
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((mediaStream) => {
+      console.log("this is a media stream: ", mediaStream);
+      const video = document.querySelector("video");
+      console.log("this is a video ele: ", video);
+      video.srcObject = mediaStream;
+      video.onloadedmetadata = () => {
+        video.play();
+      };
+    })
+    .catch((err) => {
+      // always check for errors at the end.
+      console.error(`${err.name}: ${err.message}`);
+    });
+}
